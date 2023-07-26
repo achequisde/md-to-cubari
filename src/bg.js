9d877d042eb97487e48accc3b35b96a8e51e60d7
Humberto Rondón <therealhjrp@gmail.com>
2023-07-26 10:59:19 -0400

    Add manifest.json and background file

---
+++ src/bg.js
@@ -0,0 +1,34 @@
+const API_MANGADEX_BASE_URL = "https://api.mangadex.org";
+const CUBARI_MANGADEX_BASE_URL = "https://cubari.moe/read/mangadex";
+
+function getHashFromUrl(url) {
+    const pathname = new URL(url).pathname;
+    const id = pathname.match(/[\w-]+/g)[1];
+    
+    return id;
+}
+
+browser.webRequest.onBeforeRequest.addListener((req) => {
+    const id = getHashFromUrl(req.url);
+
+    return {
+        redirectUrl: `${CUBARI_MANGADEX_BASE_URL}/${id}`,
+    };
+
+}, { urls: ["*://mangadex.org/title/*"], types: ["main_frame"] },
+    ["blocking"]);
+
+browser.webRequest.onBeforeRequest.addListener(async (req) => {
+    const id = getHashFromUrl(req.url);
+
+    const { data } = await fetch(`${API_MANGADEX_BASE_URL}/${id}`).then(res => res.json());
+
+    const chapter = data.attributes.chapter;
+    const manga = data.relationships.find(rel => rel.type === "manga");
+
+    return {
+        redirectUrl: `${CUBARI_MANGADEX_BASE_URL}/${manga.id}/${chapter}`,
+    };
+
+}, { urls: ["*://mangadex.org/chapter/*"], types: ["main_frame"] },
+    ["blocking"]);
